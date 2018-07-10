c4b34c4d038e5f8129008aa4767b8c8f1664fb83
Edwin Rodríguez <edwrodrig@gmail.com>
2018-07-10 17:30:49 -0400

    Report html table test

--- src/view/HtmlTable.php
+++ src/view/HtmlTable.php
@@ -25,32 +25,78 @@ class HtmlTable
         $this->report = $report;
     }
 
-    public function processErrorCell(ReportColumnError $error_column) {?>
-        <td style="color:red"><?=$error_column->getError()->getMessage()?></td>
-    <?php
+    /**
+     * Print the report output.
+     *
+     * This creates a ready html table. This includes table, thead and tbody tags.
+     * @uses HtmlTable::printHeaders()
+     * @uses HtmlTable::printBody()
+     */
+    public function print() {?>
+        <table>
+        <thead>
+        <?php $this->printHeaders()?>
+        </thead>
+        <tbody>
+        <?php $this->printBody()?>
+        </tbody>
+        </table>
+        <?php
     }
 
-    public function print() { ?>
-    <thead>
+    /**
+     * This print the table headers.
+     *
+     * Excludes the thead tag
+     * @see https://www.w3schools.com/tags/tag_thead.asp thead
+     */
+    public function printHeaders() {?>
         <tr>
         <?php foreach ( $this->report->getHeaders() as $header ) :?>
             <th><?=$header?></th>
         <?php endforeach ?>
         </tr>
-    </thead>
-    <tbody>
-    <?php foreach ( $this->report->getRows() as $row ) : ?>
+        <?php
+    }
+
+    /**
+     * Print the table body rows
+     *
+     * This just loops and call {@see HtmlTable::printRow}. This excludes tbody
+     * @see https://www.w3schools.com/tags/tag_tbody.asp tbody
+     */
+    public function printBody() {
+        foreach ( $this->report->getRows() as $row )
+            $this->printRow($row);
+    }
+
+    /**
+     * Print a table row
+     * @param $row
+     */
+    protected function printRow($row) {?>
         <tr>
         <?php foreach ( $row as $column_name => $column_value ) : ?>
             <?php if ( is_string($column_value)) : ?>
                 <td><?=$column_value?></td>
+            <?php elseif ( is_numeric($column_value)) : ?>
+                <td><?=$column_value?></td>
             <?php elseif ( $column_value instanceof ReportColumnError ) :
-                $this->processErrorCell($column_value);
-            endif ?>
-        <?php endforeach ?>
+                $this->printErrorCell($column_value);
+            else: ?>
+                <td></td>
+            <?php endif; ?>
+        <?php endforeach; ?>
         </tr>
-    <?php endforeach ?>
-    </tbody>
     <?php
+    }
+
+    /**
+     * Print an error row
+     * @param ReportColumnError $error_column
+     */
+    protected function printErrorCell(ReportColumnError $error_column) {?>
+        <td style="color:red"><?=$error_column->getError()->getMessage()?></td>
+        <?php
     }
 }
---
+++ tests/HtmlTableTest.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: edwin
+ * Date: 10-07-18
+ * Time: 11:59
+ */
+
+namespace test\edwrodrig\reports;
+
+use edwrodrig\reports\Report;
+use edwrodrig\reports\view\HtmlTable;
+use Exception;
+use PHPUnit\Framework\TestCase;
+use test\edwrodrig\reports\dummy\RowObject;
+
+class HtmlTableTest extends TestCase
+{
+
+    public function testPrint()
+    {
+
+        $report = new Report(RowObject::class);
+
+        $object = new RowObject;
+        $object->data = 'edwin';
+        $object->number = 1;
+
+        $report->addRow($object);
+
+        $object = new RowObject;
+        $object->data = 'amanda';
+        $object->number = 2;
+
+        $report->addRow($object);
+
+        $object = new RowObject;
+        $object->data = new Exception('NO_NAME');
+        $object->number = new Exception('NO_NUMBER');
+
+        $report->addRow($object);
+
+
+        $table = new HtmlTable($report);
+
+        ob_start();
+        $table->print();
+
+        $expected = preg_replace('/\s+/', '', <<<EOF
+        <table>
+        <thead>
+                <tr><th>data</th><th>number</th></tr>
+        </thead>
+        <tbody>
+            <tr><td>edwin</td><td>1</td></tr>
+            <tr><td>amanda</td><td>2</td></tr>
+            <tr><td style="color:red">NO_NAME</td><td style="color:red">NO_NUMBER</td></tr>
+        </tbody>
+        </table>     
+EOF
+);
+
+        $actual = preg_replace('/\s+/', '', ob_get_clean());
+        $this->assertEquals($expected, $actual);
+    }
+}
